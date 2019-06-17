580ef872aaf2a073d61a8f2537db1552dc7f3bfa
Ozgurcan Oztas <oztas.ozgurcan@gmail.com>
2019-06-17 17:12:13 +0300

    grunt completed.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,37 @@
-    module.exports = function(grunt){
+module.exports = function(grunt){
 
-        require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
-
-        //Project Configuration
-        grunt.initConfig({
-            pkg: grunt.file.readJSON('package.json'),
-            cssmin: {
-                target: {
-                    src : ['style/css-index.css', 'style/css-login.css', 'style/css-myalbums.css'],
-                    dest: 'styles.min.css'
-                }
-            },
-            uglify: {
-                options: {
-                    compress: true
-                },
-                applib: {
-                    src: [
-                        'routes/index.js',
-                        'routes/login.js',
-                        'routes/myalbums.js',
-                        'models/User.js',
-                        'config/passport.js'
-                    ],
-                    dest: 'applib.js'
-                }
+    //Project Configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less:{
+            files: {
+                'public/css/css-myalbums.css': 'public/css/less-layout.less'
             }
-        });
+        },
+        cssmin: {
+            target: {
+                src : ['public/css/css-index.css', 'public/css/css-login.css', 'public/css/css-myalbums.css'],
+                dest: 'public/styles-min.css'
+            }
+            },
+        jshint: {
+            // define the files to lint
+            files: ['Gruntfile.js', 'src/**/*.js']
+        },
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint']
+        }
+    });
 
-        // Default task
-        grunt.registerTask('default', ['uglify','cssmin']);
-    };
+    // Load npm tasks
+
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    // Default task
+    grunt.registerTask('default', ['less', 'cssmin', 'jshint']);
+};
