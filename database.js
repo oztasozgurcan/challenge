8bf2989c66df1128f4e55cbdaab85c700341b93c
Ozgurcan Oztas <oztas.ozgurcan@gmail.com>
2019-06-18 10:40:40 +0300

    package.json updated

--- database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.set('useFindAndModify', false);
-mongoose.connect('mongodb+srv://admin:ironMan26092013@challengedb-ahitm.mongodb.net/test?retryWrites=true&w=majority', {
-    useCreateIndex: true,
-    useNewUrlParser: true
-})
-    .then(db => console.log('DB is connected'))
-    .catch(err => console.error(err));
